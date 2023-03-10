eef65dbfd196d5406148cf468b3a57e0ae59fa3b
Dushant-Bansal <98218664+Dushant-Bansal@users.noreply.github.com>
2023-03-10 16:01:02 +0530

    Update auth.js

--- middleware/auth.js
+++ middleware/auth.js
@@ -2,8 +2,8 @@ const jwt = require('jsonwebtoken');
 
 module.exports = function (req, res, next) {
     try {
-        const token = req.headers.authorization.split(" ")[1];
-        // const token = req.headers.authorization;
+        // const token = req.headers.authorization.split(" ")[1];
+        const token = req.headers.authorization;
         // const token = req.header('x-auth-token');
         // const {token} = req.cookies;
         console.log(token);
